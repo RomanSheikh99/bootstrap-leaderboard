163310003de7873814b18ebb2234a1ff39129a30
roman <romansheikh170@gmail.com>
2021-08-20 00:44:25 +0600

    --- Auto-Git Commit ---

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ for(let player of players){
     player.style.borderRadius = '5px';
     player.style.margin = '0px';
 }
+
+document.getElementById('list-items').classList.add('list-group')
+
+let items = document.querySelectorAll('#list-items li');
+for(let item of items){
+    item.classList.add('list-group-item');
+    item.classList.add('list-group-item-info');
+    item.classList.add('list-group-item-action');
+}
+
+
+document.getElementById('add-player').addEventListener('click',function(){
+    let list = document.createElement('li');
+    let name = document.getElementById('player-name').value;
+    if(name == ''){
+        document.getElementById('player-name').value = 'Please Enter a Name';
+    }
+    else{
+        list.innerText = name;
+        document.getElementById('list-items').appendChild(list);
+        document.getElementById('player-name').value = '';
+    }
+    item.classList.add('list-group-item');
+    item.classList.add('list-group-item-info');
+    item.classList.add('list-group-item-action');
+})
